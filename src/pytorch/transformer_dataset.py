fb718165d9d8ca6dbce9631eeda29be9dd87a68a
Alexander Veysov <aveysov@gmail.com>
2018-10-10 16:16:41 +0300

    Add dataset for the transformer model
    [https://github.com/snakers4/cft-contest-2018/issues/1]

--- src/pytorch/transformer_dataset.py
+++ src/pytorch/transformer_dataset.py
@@ -2,86 +2,98 @@ import torch
 import numpy as np
 import pandas as pd
 import torch.utils.data as data
+from pytorch.symbols import symbol_list
 from sklearn.model_selection import StratifiedKFold
 
-from utils.text_utils.text_utils import (
-    pckl,
-    upkl
-    )
+symbol_dict = dict(zip(sorted(symbol_list),
+                       range(0,len(symbol_list))))
 
-class TransformerDataset(data.Dataset):
+class MisprintDataset(data.Dataset):
     def __init__(self,
-                 df_path='data/all_corpuses_2018_09_06_processed_le.feather', 
-                 w2i_path='data/2018_09_07_encoding_w2i.pickle',
+                 train_df_path='../data/train.csv',
+                 test_df_path='../data/test.csv',
+                 symbol_dict=symbol_dict, # only symbols in the pre-defined dict
                  mode='train', # train, val or test
-                 max_len=10,
+                 
+                 max_len=100, # good enough length for attention
                  fold=0,
-                 target_column='s_id_encoded',
-                 keyword_column='keyword',
-                 source_column='source',
-                 trainval_sources =['SEO_cluster','SEO_dash','Synonyms'],
-                 test_source=['order','select','tag'],
+                 
+                 target_column='target',
+                 name_column='fullname',
+                 gt_name_column='fullname_true',
+                 country_column='country',
+                 
                  random_state=42,
                  return_meta=False,
                  return_string=False):
         
-        df = pd.read_feather(df_path)
-        df['len'] = df[keyword_column].apply(lambda x: len(x.split()))
-        
-        train_df = df[df[source_column].isin(trainval_sources)]
-        test_df = df[df[source_column].isin(test_source)]
-        del df
-        
-        self.w2i = upkl(w2i_path)
+        train_df = pd.read_csv(train_df_path)
+        test_df = pd.read_csv(test_df_path)
+        self.s2i = symbol_dict
         
         self.mode = mode
         self.fold = fold
-        self.return_meta = return_meta
-        self.return_string = return_string
+        self.max_len = max_len
         self.random_state = random_state
 
-        self.train_texts = train_df[keyword_column].values
+        
+        cns = train_df.country.value_counts()
+        top_countries = list(cns[cns>100].index)
+        
+        train_df['country'] = train_df['country'].apply(lambda x: x if x in top_countries else 'OTHER')
+        train_df.loc[pd.isnull(train_df.fullname_true),
+                     'fullname_true'] = train_df.loc[pd.isnull(train_df.fullname_true),
+                                                     'fullname']
+        
+        test_df['country'] = test_df['country'].apply(lambda x: x if x in top_countries else 'OTHER')
+
+        self.cn_dict = dict(zip(sorted(list(train_df['country'].unique())),
+                                range(train_df['country'].nunique())))
+
+        train_df['country'] = train_df['country'].apply(lambda x: self.cn_dict[x])
+        test_df['country'] = test_df['country'].apply(lambda x: self.cn_dict[x])        
+        
+
+        self.train_names = train_df[name_column].values
+        self.train_gt_names = train_df[gt_name_column].values
         self.train_targets = train_df[target_column].values
-        self.train_lens = train_df['len'].values
-        self.train_sources = train_df[source_column].values
+        self.train_countries = train_df[country_column].values
         
-        self.test_texts = test_df[keyword_column].values
-        self.test_targets = test_df[target_column].values
-        self.test_lens = test_df['len'].values
-        self.test_sources = test_df[source_column].values
-        
-        self.max_len = max_len
+        self.test_names = test_df[name_column].values
+        self.test_countries = test_df[country_column].values
+
+        del train_df,test_df
         
         # check that unique tokens are present
         # otherwise add them to the dataset
         
         for new_tag in ['_start_','_pad_','_unk_','_classify_']:
-            if new_tag not in self.w2i:
-                self.w2i[new_tag] = len(self.w2i)
+            if new_tag not in self.s2i:
+                self.s2i[new_tag] = len(self.s2i)
                 print('{} tag added to the dataset with index {}'.format(new_tag,
-                                                                         len(self.w2i)-1))               
-        self.clf_token = self.w2i['_classify_']
-        self.vocab_len = len(self.w2i)     
+                                                                         len(self.s2i)-1))               
+        self.clf_token = self.s2i['_classify_']
+        self.vocab_len = len(self.s2i)     
             
         # add position embeddings
         for i in range(0,self.max_len):
             pos_emb = '_pos{}_'.format(i)
-            if pos_emb not in self.w2i:
-                self.w2i[pos_emb] = len(self.w2i)
+            if pos_emb not in self.s2i:
+                self.s2i[pos_emb] = len(self.s2i)
                 print('{} tag added to the dataset with index {}'.format(pos_emb,
-                                                                     len(self.w2i)-1))                   
+                                                                     len(self.s2i)-1))                   
 
         skf = StratifiedKFold(n_splits = 4,
                               shuffle = True,
                               random_state = self.random_state)
-        """
-        f1, f2, f3, f4 = skf.split(list(self.train_texts),
-                                   list(self.train_targets))
+
+        f1, f2, f3, f4 = skf.split(list(self.train_names),
+                                   list(self.train_countries))
  
         folds = [f1, f2, f3, f4]
         self.train_idx = folds[self.fold][0]
         self.val_idx = folds[self.fold][1] 
-        """
+
     def __getitem__(self, idx):
         if self.mode == 'train':
             _idx = self.train_idx[idx]
@@ -89,38 +101,54 @@ class TransformerDataset(data.Dataset):
             _idx = self.val_idx[idx]
 
         if self.mode in ('train','val'):
-            text,target,_len,source = (self.train_texts[_idx],
-                                       self.train_targets[_idx],
-                                       self.train_lens[_idx],
-                                       self.train_sources[_idx])
+            name,gt_name,target,country = (self.train_names[_idx],
+                                           self.train_gt_names[_idx],
+                                           self.train_targets[_idx],
+                                           self.train_countries[_idx])
+            
+            name_processed,name_mmb = self.process_name(name)
+            gt_name_processed,gt_name_mmb = self.process_name(gt_name)  
+            
+            return_tuple = [name,
+                            name_processed.long(),
+                            name_mmb.float(),
+                            gt_name,
+                            gt_name_processed,
+                            gt_name_mmb,
+                            target,
+                            country]
             
         elif self.mode == 'test':
-            text, target,_len,source = (self.test_texts[idx],
-                                        self.test_targets[idx],
-                                        self.test_lens[idx],
-                                        self.test_sources[idx],
-                                       )
-        original_text = text
-        text = text.split()
+            # idx, not _idx
+            name,country = (self.train_names[idx],
+                                           self.test_countries[idx])
+            
+            name_processed,name_mmb = self.process_name(name)
+            
+            return_tuple = [name,
+                            name_processed.long(),
+                            name_mmb.float(),
+                            country]            
         
-        # one token is "lost" for _classify_ token
-        # one toke is "lost" for _start_ token 
-        if len(text) > self.max_len-2:
-            text = text[:self.max_len-2]
-
+        return return_tuple
+    
+    def process_name(self,name):
         # start with the _start_ token
-        processed = [self.w2i['_start_']] * 1              
+        processed = [self.s2i['_start_']] * 1              
 
-        processed.extend([self.w2i[w] if w in self.w2i
-                          else self.w2i['_unk_'] for w in text])
-
-        # add classification token for the transformer model after each phrase
-        processed.extend([self.w2i['_classify_']] * 1)         
+        processed.extend([self.s2i[s] 
+                          if s in self.s2i
+                          else self.s2i['_unk_']
+                          for s in name])
+        
+        # add classification token for the transformer model after the end of the name
+        processed.extend([self.s2i['_classify_']] * 1)         
         
         l = len(processed)
         
         if len(processed) < self.max_len:
-            processed.extend([self.w2i['_pad_']] * (self.max_len - len(processed)))
+            processed.extend([self.s2i['_pad_']] 
+                             * (self.max_len - len(processed)))
         
         processed = torch.tensor(processed).squeeze()
         
@@ -134,19 +162,10 @@ class TransformerDataset(data.Dataset):
         # also account for the classification token
         
         pmb = np.arange(self.vocab_len, self.vocab_len + self.max_len)
-        
         processed = torch.stack((processed,torch.from_numpy(pmb)),dim=1)
         
-        if self.return_meta:
-            return_tuple = [processed.float(),mmb.float(),target,_len,source]
-        else:
-            return_tuple = [processed.float(),mmb.float(),target]
-            
-        if self.return_string:
-            return_tuple.append(original_text)
+        return processed,mmb
         
-        return return_tuple
-    
     def __len__(self):
         if self.mode == 'train':
             return len(self.train_idx)
